784a4f475c43912e58b5472022a29b1cd7c83b66
David Welton <david.welton@dutchie.com>
2020-04-16 11:11:29 -0700

    Ripped out cache

--- lib/ipinfo.rb
+++ lib/ipinfo.rb
@@ -3,15 +3,12 @@
 require 'cgi'
 require 'ipaddr'
 require 'ipinfo/adapter'
-require 'ipinfo/cache/default_cache'
 require 'ipinfo/errors'
 require 'ipinfo/response'
 require 'ipinfo/version'
 require 'json'
 
 module IPinfo
-  DEFAULT_CACHE_MAXSIZE = 4096
-  DEFAULT_CACHE_TTL = 60 * 60 * 24
   DEFAULT_COUNTRY_FILE = File.join(File.dirname(__FILE__), 'ipinfo/countries.json')
   RATE_LIMIT_MESSAGE = "To increase your limits, please review our paid plans at https://ipinfo.io/pricing"
 
@@ -29,10 +26,6 @@ module IPinfo
     def initialize(access_token=nil, settings={})
       @access_token = access_token
       @http_client = http_client(settings.fetch("http_client", nil))
-
-      maxsize = settings.fetch("maxsize", DEFAULT_CACHE_MAXSIZE)
-      ttl = settings.fetch("ttl", DEFAULT_CACHE_TTL)
-      @cache = settings.fetch("cache", DefaultCache.new(ttl, maxsize))
       @countries = countries(settings.fetch('countries', DEFAULT_COUNTRY_FILE))
     end
 
@@ -57,15 +50,12 @@ module IPinfo
 
     protected
     def request_details(ip_address=nil)
-      if !@cache.contains?(ip_address)
-        response = @http_client.get(escape_path(ip_address))
+      response = @http_client.get(escape_path(ip_address))
 
-        raise RateLimitError.new(RATE_LIMIT_MESSAGE) if response.status.eql?(429)
+      raise RateLimitError.new(RATE_LIMIT_MESSAGE) if response.status.eql?(429)
 
-        details = JSON.parse(response.body, symbolize_names: true)
-        @cache.set(ip_address, details)
-      end
-      @cache.get(ip_address)
+      details = JSON.parse(response.body, symbolize_names: true)
+      details
     end
 
     def http_client(http_client=nil)
